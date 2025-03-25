8cbdd150fa0e70e299ebcfa5bfa143c9f4ba548f
supriyapandey93 <supriyapade11@gmail.com>
2025-03-25 07:33:12 +0530

    Added backend folder

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+connectDB(); // 🔥 Isse server start hone se pehle database connect hoga
+
+const app = express();
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
