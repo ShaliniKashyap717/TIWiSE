409453e06375c0f7dbcf84ddb0532fd73022f745
supriyapandey93 <supriyapade11@gmail.com>
2025-04-10 21:37:59 +0530

    added

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 // ✅ Import the environment variable
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+console.log(BACKEND_URL)
 
 export default axios.create({
   baseURL: BACKEND_URL,
